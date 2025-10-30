45d13f35679707da56ff503601c872e4a96ec6b1
dannysir <dannysir@naver.com>
2025-10-30 19:09:28 +0900

    test : User 모델과 Lotto모델의 유효성 검사 로직 추가
    Lotto클래스 위치 변경으로 import문 수정.
    User모델에서 금액에 따른 유효성 검사 로직 추가.

--- __tests__/unitTest.js
+++ __tests__/unitTest.js
@@ -1,18 +1,25 @@
-import Lotto from "../src/models/Lotto.js";
+import { User } from '../src/models/User.js';
 
-describe("로또 클래스 테스트", () => {
+describe('User 모델', () => {
+  test.each([[100], ['1!99'], [' 100'], ['1001'], ['40001']])(
+    '[예외 테스트] 잘못된 금액 - 입력 : %s',
+    async (input) => {
+      await expect(new User(input)).rejects.toThrow('[ERROR]');
+    }
+  );
+
+});
+
+describe('Lotto 모델', () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
-
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 });

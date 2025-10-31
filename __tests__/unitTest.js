366231ec0a8dcf39bc58eb327376110b3a523e6d
dannysir <dannysir@naver.com>
2025-10-31 12:11:06 +0900

    test : User 랜덤 로또 생성 테스트 코드 작성
    User가 자신의 돈만큼의 로또 구매.
    mockRandoms를 이용해 랜덤값을 제어하여 테스트 진행.

--- __tests__/unitTest.js
+++ __tests__/unitTest.js
@@ -1,4 +1,6 @@
 import { User } from '../src/models/User.js';
+import { Lotto } from '../src/models/Lotto.js';
+import { mockRandoms } from './helpers/mockUtils.js';
 
 describe('User 모델', () => {
   test.each([[100], ['1!99'], [' 100'], ['1001'], ['40001']])(
@@ -8,18 +10,27 @@ describe('User 모델', () => {
     }
   );
 
+  test('3개의 로또 생성', () => {
+    const money = 3000;
+    const randoms = [
+      [1, 2, 3, 4, 5, 6],
+      [3, 12, 18, 27, 35, 41],
+      [1, 8, 15, 22, 39, 44],
+    ];
+    mockRandoms(randoms);
+
+    const user = new User(money);
+    user.buyLotto();
+
+    expect(user.getLottoNumbers()).toEqual(randoms);
+  });
 });
 
 describe('Lotto 모델', () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
-  });
-
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+  test.each([
+    ['로또 번호 갯수 6 초과', [1, 2, 3, 4, 5, 6, 7]],
+    ['로또 번호 중복', [1, 2, 3, 4, 5, 5]],
+  ])('[예외 테스트] 로또 번호 %s - 입력 : %s', async (_, input) => {
+    await expect(() => new Lotto(input)).toThrow('[ERROR]');
   });
 });

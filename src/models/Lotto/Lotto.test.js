e0fef5a25ad6c5d1030b774622595d4607ff6ac8
dannysir <dannysir@naver.com>
2025-11-01 16:33:36 +0900

    test : Lotto 유닛 테스트 추가
    2등이 아닌 보너스 번호 포함 테스트 추가.

--- src/models/Lotto/Lotto.test.js
+++ src/models/Lotto/Lotto.test.js
@@ -7,4 +7,16 @@ describe('Lotto 모델', () => {
   ])('[예외 테스트] 로또 번호 %s - 입력 : %s', (_, input) => {
     expect(() => new Lotto(input)).toThrow('[ERROR]');
   });
+
+  test('일치 번호 카운트 메서드', () => {
+    const duplicate = new Set([1, 2, 3, 4, 5, 6, 45]);
+    const bonus = 45;
+    const input = '1,2,3,4,10,45';
+    const count = 5;
+    const isBonus = true;
+
+    const lotto = new Lotto(input.split(','));
+
+    expect(lotto.check(duplicate, bonus)).toEqual([count, isBonus]);
+  });
 });

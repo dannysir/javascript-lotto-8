6bace6e650b00046c52d7c2490c7b63528fb1d8d
dannysir <dannysir@naver.com>
2025-11-01 16:34:47 +0900

    test : User 정상 동작 유닛 테스트 추가
    2등이 아닌 보너스 번호 포함 테스트 추가.
    beforeEach를 통해 정상 입력 로직 추가.

--- src/models/User/User.test.js
+++ src/models/User/User.test.js
@@ -9,17 +9,35 @@ describe('User 모델', () => {
     }
   );
 
-  test('3개의 로또 생성', () => {
-    const money = '3000';
-    const randoms = [
-      [1, 2, 3, 4, 5, 6],
-      [3, 12, 18, 27, 35, 41],
-      [1, 8, 15, 22, 39, 44],
-    ];
-    mockRandoms(randoms);
+  describe('정상 동작 메서드 테스트', () => {
+    let user;
+    let randoms;
 
-    const user = new User(money);
+    beforeEach(() => {
+      jest.restoreAllMocks();
+      const money = '3000';
+      randoms = [
+        [1, 2, 3, 4, 5, 6],
+        [1, 2, 3, 4, 5, 45],
+        [1, 2, 3, 4, 10, 45],
+      ];
+      mockRandoms(randoms);
 
-    expect(user.getLottoNumbers()).toEqual(randoms);
+      user = new User(money);
+    });
+
+    test('3개의 로또 생성', () => {
+      expect(user.getLottoNumbers()).toEqual(randoms);
+    });
+
+    test('로또 결과 리턴', () => {
+      const resultArr = [1, 1, 0, 1, 0];
+      const duplicate = new Set([1, 2, 3, 4, 5, 6, 45]);
+      const bonus = 45;
+      const profit = '67668333.3';
+
+      expect(user.result(duplicate, bonus)).toEqual([resultArr, profit]);
+    });
   });
+
 });
